12fd1668c10d5f5661a7f5171362f1e66391f077
dspda <dspda@172.16.44.89>
2019-01-08 18:11:20 -0800

    Initial Commit

---
+++ clinic-users/clinic-users/src/main/java/com/clinicusers/ClinicUsersApplication.java
@@ -0,0 +1,14 @@
+package com.clinicusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicUsersApplication.class, args);
+	}
+
+}
+
